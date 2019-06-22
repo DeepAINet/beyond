2a2ba0e2bdba2954e78fc84cb1c4cedd3852656a
mengqy <mengqy@mengqydeMacBook-Pro.local>
2019-06-22 22:37:15 +0800

    add dot_mul interface.

--- src/global_ops.h
+++ src/global_ops.h
@@ -5,8 +5,13 @@
 #ifndef BEYOND_GLOBAL_OPS_H
 #define BEYOND_GLOBAL_OPS_H
 
-namespace glops{
 
+#include "variable.h"
+
+namespace glops{
+    variable& dot_mul(variable& a, variable& b, bool a_transpose, bool b_transpose){
+
+    }
 }
 
 #endif //BEYOND_GLOBAL_OPS_H
