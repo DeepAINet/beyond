806b6ed614ba61a4b4ce6c0f583428620bf14db8
mengqy <mengqy@mengqydeMacBook-Pro.local>
2019-06-22 22:24:49 +0800

    modify global variable names.

--- src/global.h
+++ src/global.h
@@ -11,19 +11,21 @@ map<string, tensor<real>*> grad_tables;
 
 int global_steps = 0;
 
-int total_steps = 0;
+int train_steps_ = 0;
 
 int current_epoch = 0;
 
-int epoch_num = 1;
+int epoch_num_ = 1;
 
-int evaluate_steps = 1000;
+int eval_steps_ = 1000;
+
+int checkpoint_steps_ = 10000;
 
 int print_steps = 10;
 
-int batch_size = 64;
+int batch_size_ = 64;
 
-int num_threads = 8;
+int num_threads_ = 8;
 
 real start_learning_rate = 0.01;
 
@@ -51,11 +53,11 @@ inline string partial_derivative_name(string name){
 void print_hyper_params(){
     std::cout << "***********************************************" << std::endl
               << "\t-start learning rate: " << start_learning_rate << std::endl
-              << "\t-batch size: " << batch_size << std::endl
-              << "\t-epoch num: " << epoch_num << std::endl
-              << "\t-evaluate steps: " << evaluate_steps << std::endl
+              << "\t-batch size: " << batch_size_ << std::endl
+              << "\t-epoch num: " << epoch_num_ << std::endl
+              << "\t-evaluate steps: " << eval_steps_ << std::endl
               << "\t-print steps: " << print_steps << std::endl
-              << "\t-num thread: " << num_threads << std::endl
+              << "\t-num thread: " << num_threads_ << std::endl
               << "***********************************************" << std::endl;
 }
 
