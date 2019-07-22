49d19750102d10879ad4714d58297849e30be61a
mengqy <mengqingyang0102@163.com>
2019-07-22 22:59:35 +0800

    add constant

---
+++ src/constant.h
@@ -0,0 +1,15 @@
+//
+// Created by mengqy on 2019/7/19.
+//
+
+#ifndef BEYOND_CONSTANT_H
+#define BEYOND_CONSTANT_H
+
+#endif //BEYOND_CONSTANT_H
+
+#include <string>
+using namespace std;
+
+string BATCH_X = "batch_x";
+
+string BATCH_Y = 'batch_y';
